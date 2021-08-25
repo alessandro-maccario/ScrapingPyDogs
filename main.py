7a269897190d45f59a76569aeaf1f66ef0d67ecb
alessandro-maccario <alessandro.maccario@gmail.com>
2021-08-25 23:47:58 +0200

    First commit - A

---
+++ main.py
@@ -0,0 +1,21 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+##### CONNECT TO THE WEB SITE #####
+url = "https://dogtime.com/dog-breeds/profiles"
+# Open the URL as Browser, not as python urllib
+page = urllib.request.Request(url, headers = {'User-Agent': 'Mozilla/5.0'})
+infile = urllib.request.urlopen(page).read()
+data = infile.decode('ISO-8859-1') # Read the content as string decoded with ISO-8859-1
+
+##### USE OF BEAUTIFUL SOUP #####
+# print(data) # Print the data to the screen
+
+soup = BeautifulSoup(data, 'html.parser')
+
+##### CREATE A PANDAS SERIES TO TAKE CARE OF ALL THE DOGS NAME #####
+for name in soup.find_all("a", class_="list-item-title"):
+    print(name.text)
+
+
+
