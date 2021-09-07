1e8e204eb0d89e96eb402fd8f95f2720431eb3c1
alessandro-maccario <alessandro.maccario@gmail.com>
2021-09-07 19:03:22 +0200

    attempt to save to MYSQL done

--- from_df_to_SQL.py
+++ from_df_to_SQL.py
@@ -1,4 +1,5 @@
 import csv
+import pandas as pd
 import mysql.connector
 from mysql.connector import errorcode
 
@@ -25,63 +26,75 @@ cursor.execute(use_db)
 
 ################################################
 # CREATE TABLES
+# TABLES = dict()
+# TABLES['breeds'] = (
+#     ''' CREATE TABLE IF NOT EXISTS `dogs_scraping`.`breeds` (
+#      `id` INT(10) UNSIGNED UNIQUE NOT NULL AUTO_INCREMENT,
+#      `id_breed_group` INT(10) UNSIGNED NOT NULL,
+#      `name` VARCHAR(100) NOT NULL,
+#      `description` TEXT NOT NULL,
+#      `url_image` VARCHAR(256) NOT NULL,
+#      `adaptability_id` INT(10) UNSIGNED NOT NULL,
+#      `height` VARCHAR(100) NULL DEFAULT NULL,
+#      `weight` VARCHAR(100) NULL DEFAULT NULL,
+#      `life_span` VARCHAR(100) NULL DEFAULT NULL,
+#      PRIMARY KEY (`id`),
+#      UNIQUE INDEX `name` (`name` ASC),
+#      INDEX `fk_breed_in_group_idx` (`id_breed_group` ASC),
+#      INDEX `fk_breeds_adaptability1_idx` (`adaptability_id` ASC),
+#      CONSTRAINT `fk_breed_in_group` FOREIGN KEY (`id_breed_group`)
+#        REFERENCES `dogs`.`breeds_group` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+#      CONSTRAINT `fk_breeds_adaptability1` FOREIGN KEY (`adaptability_id`)
+#        REFERENCES `dogs`.`adaptability` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+#     ) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARACTER SET = utf8 ''')
+#
+# TABLES['breeds_group'] = (
+#     ''' CREATE TABLE IF NOT EXISTS `dogs_scraping`.`breeds_group` (
+#         `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+#         `name` VARCHAR(100) NOT NULL,
+#         PRIMARY KEY (`id`),
+#         UNIQUE INDEX `name` (`name` ASC)) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARACTER SET = utf8 ''')
+#
+# TABLES['adaptability'] = (
+#    ''' CREATE TABLE IF NOT EXISTS `dogs_scraping`.`adaptability` (
+#     `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+#     `apartment_living` INT(11) NOT NULL,
+#     `good_for_novice_owner` TEXT NOT NULL,
+#     `sensitivity` INT(11) NOT NULL,
+#     `tolerates_being_alone` INT(11) NOT NULL,
+#     `tolerates_cold_weather` INT(11) NOT NULL,
+#     `tolerates_hot_weather` INT(11) NOT NULL,
+#     PRIMARY KEY (`id`)) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARACTER SET = utf8 ''')
+
+
+##################################################
+# CREATE TABLE
 TABLES = {}
 TABLES['breeds'] = (
     ''' CREATE TABLE IF NOT EXISTS `dogs_scraping`.`breeds` (
-     `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-     `id_breed_group` INT(10) UNSIGNED NOT NULL,
-     `name` VARCHAR(100) NOT NULL,
+     `id` INT(10) UNIQUE NOT NULL AUTO_INCREMENT,
+     `name` VARCHAR(100) UNIQUE NOT NULL,
      `description` TEXT NOT NULL,
-     `url_image` VARCHAR(256) NOT NULL,
-     `adaptability_id` INT(10) UNSIGNED NOT NULL,
-     `height` VARCHAR(100) NULL DEFAULT NULL,
-     `weight` VARCHAR(100) NULL DEFAULT NULL,
-     `life_span` VARCHAR(100) NULL DEFAULT NULL,
-     PRIMARY KEY (`id`),
-     UNIQUE INDEX `name` (`name` ASC),
-     INDEX `fk_breed_in_group_idx` (`id_breed_group` ASC),
-     INDEX `fk_breeds_adaptability1_idx` (`adaptability_id` ASC),
-     CONSTRAINT `fk_breed_in_group` FOREIGN KEY (`id_breed_group`)
-       REFERENCES `dogs`.`breeds_group` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-     CONSTRAINT `fk_breeds_adaptability1` FOREIGN KEY (`adaptability_id`)
-       REFERENCES `dogs`.`adaptability` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-    ) ENGINE = InnoDB AUTO_INCREMENT = 3 DEFAULT CHARACTER SET = utf8 ''')
-
-TABLES['breeds_group'] = (
-    ''' CREATE TABLE IF NOT EXISTS `dogs_scraping`.`breeds_group` (
-        `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-        `name` VARCHAR(100) NOT NULL,
-        PRIMARY KEY (`id`),
-        UNIQUE INDEX `name` (`name` ASC)) ENGINE = InnoDB AUTO_INCREMENT = 3 DEFAULT CHARACTER SET = utf8 ''')
-
-TABLES['adaptability'] = (
-   ''' CREATE TABLE IF NOT EXISTS `dogs_scraping`.`adaptability` (
-    `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `apartment_living` INT(11) NOT NULL,
-    `good_for_novice_owner` TEXT NOT NULL,
-    `sensitivity` INT(11) NOT NULL,
-    `tolerates_being_alone` INT(11) NOT NULL,
-    `tolerates_cold_weather` INT(11) NOT NULL,
-    `tolerates_hot_weather` INT(11) NOT NULL,
-    PRIMARY KEY (`id`)) ENGINE = InnoDB AUTO_INCREMENT = 3 DEFAULT CHARACTER SET = utf8 ''')
+     PRIMARY KEY (`id`)
+    ) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARACTER SET = utf8 ''')
 
 ##################################################
 
 # FOR EACH TABLE IN TABLES CREATE TABLE AND HANDLING EXCEPTION
 for table_name in TABLES:
-   table_description = TABLES[table_name]
-   try:
-       print("WAITING PLEASE...")
-       cursor.execute(table_description)
-       print(f"Creating table {table_name}: ", end='')
-   except mysql.connector.Error as err:
-       if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
-           print("already exists.")
-       else:
-           print(err.msg)
-   else:
-       print("OK")
-       print("--- --- --- --- --- --- --- --- --- ---")
+    table_description = TABLES[table_name]
+    try:
+        print("WAITING PLEASE...")
+        cursor.execute(table_description)
+        print(f"Creating table {table_name}: ", end='')
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+            print("already exists.")
+        else:
+            print(err.msg)
+    else:
+        print("OK")
+        print("--- --- --- --- --- --- --- --- --- ---")
 
 ################################################
 
@@ -99,17 +112,31 @@ for table_name in TABLES:
 ## - THEN YOU HAVE YOUR ID_BREED_GROUP = SOME_NUMBER
 ## YOU CAN TRY WITH SOMETHING LIKE THIS:
 
-strSQL = "INSERT INTO `dogs`.`breeds` (name, description, url_image, height, weight, life_span )" + \
-         " VALUES(%s, %s, %s, %s, %s, %s)"
+################################################
+# READ THE DATAFRAME
+csv_data = pd.read_csv('out.csv', header=0, encoding = 'utf8', delimiter=",", sep=' *, *', skipinitialspace = True)
 
-with open('out.csv', 'r') as f:
-    csv_data = csv.reader(f)
-    for row in csv_data:
-        cursor.execute(strSQL, row)
-        conn.commit()
+# TAKE ONLY THE FIRST COLUMN (NAME)
+dogs_name = csv_data.iloc[:,0]
 
-print("Done")
+# TAKE ONLY THE SECOND COLUMN (DESCRIPTION)
+dogs_description = csv_data.iloc[:,1]
+
+# CONCATENATE SERIES AND CONVERT TO DATAFRAME
+dogs_df = pd.concat([dogs_name, dogs_description], axis=1)
+
+# QUERIES
+to_SQL = """INSERT INTO `dogs_scraping`.`breeds` (name, description) VALUES (%s, %s)"""
+
+# CYCLE THROUGH DATAFRAME AND SAVE ROWS TO MYSQL
+for index, row in dogs_df.iterrows():
+    try:
+        cursor.execute(to_SQL, ( row['name'], row['description'], ))
+        cnx.commit()
+    except:
+        print("MISSING SOMETHING!")
+
+print("INSERTING OPERATION: DONE")
 
 cursor.close()
-conn.close()
-
+################################################
